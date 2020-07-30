529a73482b2dc414dd54e90d9d7d743a8533515e
Rishab Baldua <rishabbaldua@rishabs-air.lan>
2020-07-30 16:22:05 -0700

    form submit part 3

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -18,14 +18,75 @@ class Navbar extends Component {
      
         };
         this.handleNameChange = this.handleNameChange.bind(this);
-
+        this.handleClick = this.handleClick.bind(this);
+        this.handlePhoneChange = this.handlePhoneChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handleAddressChange = this.handleAddressChange.bind(this);
+        this.handleCityChange = this.handleCityChange.bind(this);
+        this.handleStateChange = this.handleStateChange.bind(this);
+        this.handleZipChange = this.handleZipChange.bind(this);
+        this.handleDetailsChange = this.handleDetailsChange.bind(this);
+        this.handleAvailabilityChange = this.handleAvailabilityChange.bind(this);
       }
      
-     handleNameChange(e){
+    handleNameChange(e){
         this.setState({name: e.target.value});
         console.log("name handle clicked", this.state.name);
     }
 
+    handlePhoneChange(e){
+      this.setState({phone: e.target.value});
+      console.log("phone handle clicked", this.state.phone);
+    }
+
+    handleEmailChange(e){
+      this.setState({email: e.target.value});
+      console.log("email handle clicked", this.state.email);
+    }
+
+    handleAddressChange(e){
+      this.setState({address: e.target.value});
+      console.log("address handle clicked", this.state.Address);
+    }
+
+    handleCityChange(e){
+      this.setState({city: e.target.value});
+      console.log("city handle clicked", this.state.city);
+    }
+
+    handleStateChange(e){
+      this.setState({state: e.target.value});
+      console.log("state handle clicked", this.state.state);
+    }
+
+    handleZipChange(e){
+      this.setState({zip: e.target.value});
+      console.log("zip handle clicked", this.state.zip);
+    }
+
+    handleDetailsChange(e){
+      this.setState({details: e.target.value});
+      console.log("details handle clicked", this.state.details);
+    }
+
+    handleAvailabilityChange(e){
+      this.setState({availability: e.target.value});
+      console.log("availability handle clicked", this.state.availability);
+    }
+
+    handleClick() {
+      console.log("handleClick", this.state.name);
+      var requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({name: this.state.name, phone: this.state.phone, email: this.state.email, address: this.state.address, city: this.state.city, state: this.state.state, zip: this.state.zip, details: this.state.details, availability: this.state.availability })
+    };
+
+    fetch('/api/add/user', requestOptions)
+        .then(response => response.json())
+        .then(data => this.setState({ postId: data.id }));
+
+    }
     render() {
     var divStyle = {
       margin: 20,
@@ -34,22 +95,11 @@ class Navbar extends Component {
 
     
     
-    function handleClick() {
-      console.log("handleClick")
-      var requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title: 'React POST Request Example' })
-    };
-    fetch('/api/add/user', requestOptions)
-        .then(response => response.json())
-        .then(data => this.setState({ postId: data.id }));
-
-    }
+    
 
     return (
       <div style={divStyle}>
-        <Button variant="primary" onClick={this.handleNameChange}>
+        <Button variant="primary" onClick={this.handleClick}>
           test 
         </Button >
         <Form>
@@ -61,24 +111,24 @@ class Navbar extends Component {
 
             <Form.Group as={Col} controlId="formGridPhone">
               <Form.Label>Phone Number</Form.Label>
-              <Form.Control type="phone" placeholder="Enter Phone Number" />
+              <Form.Control type="phone" placeholder="Enter Phone Number" value={this.state.phone} onChange={this.handlePhoneChange}/>
             </Form.Group>
           </Form.Row>
 
           <Form.Group controlId="formGridEmail">
             <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter Email" />
+            <Form.Control type="email" placeholder="Enter Email" value={this.state.email} onChange={this.handleEmailChange}/>
           </Form.Group>
 
           <Form.Group controlId="formGridAddress1">
             <Form.Label>Address</Form.Label>
-            <Form.Control placeholder="1234 Main St" />
+            <Form.Control placeholder="1234 Main St"  value={this.state.address} onChange={this.handleAddressChange}/>
           </Form.Group>
 
           <Form.Row>
             <Form.Group as={Col} controlId="formGridCity">
               <Form.Label>City</Form.Label>
-              <Form.Control />
+              <Form.Control  value={this.state.city} onChange={this.handleCityChange}/>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridState">
@@ -91,18 +141,18 @@ class Navbar extends Component {
 
             <Form.Group as={Col} controlId="formGridZip">
               <Form.Label>Zip</Form.Label>
-              <Form.Control />
+              <Form.Control  value={this.state.zip} onChange={this.handleZipChange}/>
             </Form.Group>
           </Form.Row>
 
           <Form.Group controlId="formGridInfo">
             <Form.Label>Details</Form.Label>
-            <Form.Control as="textarea" rows="4" type="info" placeholder="Enter Type of Fruit and Number of Trees etc." />
+            <Form.Control as="textarea" rows="4" type="info" placeholder="Enter Type of Fruit and Number of Trees etc."  value={this.state.details} onChange={this.handledetailsChange} />
           </Form.Group>
 
           <Form.Group controlId="formGridAvailability">
             <Form.Label>Availability</Form.Label>
-            <Form.Control type="availability" placeholder="Enter Availability" />
+            <Form.Control type="availability" placeholder="Enter Availability"  value={this.state.availability} onChange={this.handleAvailabilityChange}/>
           </Form.Group>
         
 
